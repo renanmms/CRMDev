29b675b811908b61e948b5dcb8537d60d9bf7c74
Renan Martins <renanmartins1999@hotmail.com>
2024-08-30 22:45:01 -0300

    feat: contact endpoints
    Issue: #1

--- CRMDev.API/Program.cs
+++ CRMDev.API/Program.cs
@@ -43,6 +43,32 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+app.MapGet("/api/contacts", (CRMContext context) => {
+    var contacts = context.Contacts.ToList();
+
+    return Results.Ok(contacts);
+});
+
+app.MapGet("/api/contacts/{id}", (Guid id, CRMContext context) => {
+    var contact = context.Contacts.SingleOrDefault(c => c.Id == id);
+
+    if(contact is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(contact);
+});
+
+app.MapPost("/api/contacts", (CRMContext context, CreateContactInputModel model) => {
+    var contact = new Contact(model.Name, model.Email);
+
+    context.Add(contact);
+    context.SaveChanges();
+
+    return Results.Created($"/api/contacts/{contact.Id}", contact);
+});
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
