d735e1ab2c8f1de6fb9939130150427026098f5b
Renan Martins <renanmartins1999@hotmail.com>
2024-08-30 22:41:29 -0300

    feat: creating dbcontext

---
+++ CRMDev.API/Persistence/CRMContext.cs
@@ -0,0 +1,17 @@
+using System;
+using CRMDev.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRMDev.API.Persistence
+{
+    public class CRMContext : DbContext
+    {
+        public CRMContext(DbContextOptions<CRMContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
