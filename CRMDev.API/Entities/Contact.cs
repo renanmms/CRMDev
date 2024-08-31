a848866314386907fddd4d81e0f3667a02808ef1
Renan Martins <renanmartins1999@hotmail.com>
2024-08-30 22:41:54 -0300

    feat: adding constructor for contact
    Issue: #1

--- CRMDev.API/Entities/Contact.cs
+++ CRMDev.API/Entities/Contact.cs
@@ -2,6 +2,12 @@ namespace CRMDev.API.Entities
 {
     public class Contact
     {
+        public Contact(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
         public Guid Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
