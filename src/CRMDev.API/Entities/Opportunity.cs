3260f5d3cf6107cc518f722b64614fd51691484e
Renan Martins <renanmartins1999@hotmail.com>
2024-11-22 07:58:59 -0300

    feat: add constructor for entity

--- src/CRMDev.API/Entities/Opportunity.cs
+++ src/CRMDev.API/Entities/Opportunity.cs
@@ -4,6 +4,24 @@ namespace CRMDev.API.Entities
 {
     public class Opportunity
     {
+        public Opportunity(
+            string? title,
+            string? description,
+            DateTime dueDate,
+            decimal budget,
+            bool hasSupport,
+            StatusEnum status,
+            ReasonEnum reason)
+        {
+            Title = title;
+            Description = description;
+            DueDate = dueDate;
+            Budget = budget;
+            HasSupport = hasSupport;
+            Status = status;
+            Reason = reason;
+        }
+
         public Guid Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
