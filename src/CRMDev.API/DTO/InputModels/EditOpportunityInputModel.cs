1fdb1fd15bcce2d4d6366fae7cea1a7eefb08a5c
Renan Martins <renanmartins1999@hotmail.com>
2024-11-26 08:39:47 -0300

    feat: add dto for edit opportunity

---
+++ src/CRMDev.API/DTO/InputModels/EditOpportunityInputModel.cs
@@ -0,0 +1,16 @@
+using CRMDev.API.Entities;
+using CRMDev.API.Enums;
+
+namespace CRMDev.API.DTO.InputModels
+{
+    public class EditOpportunityInputModel
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public decimal Budget { get; set; }
+        public bool HasSupport { get; set; }
+        public StatusEnum Status { get; set; }
+        public ReasonEnum Reason { get; set; }
+    }
+}
